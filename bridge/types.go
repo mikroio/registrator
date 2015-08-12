2f63d907eee7ea96c96e7dd2b5d5abc34e8aa068
Johan Rydberg <johan.rydberg@gmail.com>
2015-08-12 20:03:11 +0200

    make it possible to ignore containers

--- bridge/types.go
+++ bridge/types.go
@@ -2,55 +2,56 @@
 package bridge
 
 import (
-	"net/url"
+  "net/url"
 
-	dockerapi "github.com/fsouza/go-dockerclient"
+  dockerapi "github.com/fsouza/go-dockerclient"
 )
 
 type AdapterFactory interface {
-	New(uri *url.URL) RegistryAdapter
+  New(uri *url.URL) RegistryAdapter
 }
 
 type RegistryAdapter interface {
-	Ping() error
-	Register(service *Service) error
-	Deregister(service *Service) error
-	Refresh(service *Service) error
+  Ping() error
+  Register(service *Service) error
+  Deregister(service *Service) error
+  Refresh(service *Service) error
 }
 
 type Config struct {
-	HostIp          string
-	Internal        bool
-	ForceTags       string
-	RefreshTtl      int
-	RefreshInterval int
-	DeregisterCheck string
+  HostIp          string
+  Internal        bool
+  ForceTags       string
+  RefreshTtl      int
+  RefreshInterval int
+  DeregisterCheck string
+  Ignore          []string
 }
 
 type Service struct {
-	ID    string
-	Name  string
-	Port  int
-	IP    string
-	Tags  []string
-	Attrs map[string]string
-	TTL   int
+  ID    string
+  Name  string
+  Port  int
+  IP    string
+  Tags  []string
+  Attrs map[string]string
+  TTL   int
 
-	Origin ServicePort
+  Origin ServicePort
 }
 
 type DeadContainer struct {
-	TTL      int
-	Services []*Service
+  TTL      int
+  Services []*Service
 }
 
 type ServicePort struct {
-	HostPort          string
-	HostIP            string
-	ExposedPort       string
-	ExposedIP         string
-	PortType          string
-	ContainerHostname string
-	ContainerID       string
-	container         *dockerapi.Container
+  HostPort          string
+  HostIP            string
+  ExposedPort       string
+  ExposedIP         string
+  PortType          string
+  ContainerHostname string
+  ContainerID       string
+  container         *dockerapi.Container
 }
